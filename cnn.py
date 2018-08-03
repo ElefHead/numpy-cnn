f25f2cc99147717e460d4c5ef91c0a39e2dffef1
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2018-08-02 22:37:40 -0500

    Finished. Updated readme

--- cnn.py
+++ cnn.py
@@ -14,8 +14,8 @@ np.random.seed(0)
 
 
 if __name__ == '__main__':
-    train_data, train_labels = get_data(num_samples=500)
-    test_data, test_labels = get_data(num_samples=500, dataset="testing")
+    train_data, train_labels = get_data(num_samples=50000)
+    test_data, test_labels = get_data(num_samples=10000, dataset="testing")
 
     train_data = train_data / 255
     test_data = test_data / 255
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     model.set_loss(CategoricalCrossEntropy)
 
-    # model.train(train_data, train_labels.T, epochs=2)
-    model.load_weights()
+    model.train(train_data, train_labels.T, epochs=2) # set load_and_continue to True if you want to start from already trained weights
+    # model.load_weights() # uncomment if loading previously trained weights and comment above line to skip training and only load trained weights.
 
     print('Testing accuracy = {}'.format(model.evaluate(test_data, test_labels)))
